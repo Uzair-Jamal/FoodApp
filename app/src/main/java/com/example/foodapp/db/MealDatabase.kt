91202a70009466b251e99d2fda87f23979f3fc12
Uzair Jamal <syeduzair99jamal@gmail.com>
2024-07-03 19:59:52 +0500

    till MealDatabase.

--- app/src/main/java/com/example/foodapp/db/MealDatabase.kt
+++ app/src/main/java/com/example/foodapp/db/MealDatabase.kt
@@ -1,9 +1,31 @@
 package com.example.foodapp.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 import com.example.foodapp.pojo.Meal
 
 @Database(entities = [Meal::class], version = 1)
-abstract class MealDatabase {
+abstract class MealDatabase:RoomDatabase()
+{
     abstract fun mealDao(): MealDao
+
+    companion object{
+        @Volatile
+        var INSTANCE: MealDatabase?= null
+
+        @Synchronized
+        fun getInstance(context: Context):MealDatabase{
+            if(INSTANCE!=null){
+                INSTANCE = Room.databaseBuilder(
+                    context,
+                    MealDatabase::class.java,
+                    "meal_db"
+                ).fallbackToDestructiveMigration()
+                    .build()
+            }
+            return INSTANCE as MealDatabase
+        }
+    }
 }
---
+++ app/src/main/java/com/example/foodapp/db/MealTypeConvertor.kt
@@ -0,0 +1,8 @@
+package com.example.foodapp.db
+
+import androidx.room.TypeConverters
+
+@TypeConverters
+class MealTypeConvertor {
+
+}
